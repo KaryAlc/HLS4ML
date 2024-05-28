64afda57896d7c76f6a96a60471879f3f46ebaa5
KaryAlc <karina.alcantara@cua.uam.mx>
2024-05-28 04:01:29 +0000

    feat: modifies in notebook 6

---
+++ pruned_cnn/firmware/weights/b26.h
@@ -0,0 +1,15 @@
+//Numpy array shape [10]
+//Min -0.109204664826
+//Max 0.152615070343
+//Number of zeros 0
+
+#ifndef B26_H_
+#define B26_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b26[10];
+#else
+output_dense_bias_t b26[10] = {-0.0522833019, 0.0247708485, 0.0112099666, -0.0207252316, -0.0207867045, -0.0181927923, 0.0422043689, -0.1092046648, 0.1526150703, 0.0464949831};
+#endif
+
+#endif
---
+++ quantized_pruned_cnn/firmware/weights/b23.h
@@ -0,0 +1,15 @@
+//Numpy array shape [10]
+//Min -0.125458091497
+//Max 0.089219748974
+//Number of zeros 0
+
+#ifndef B23_H_
+#define B23_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b23[10];
+#else
+output_dense_bias_t b23[10] = {-0.1254580915, 0.0505750217, 0.0236531850, 0.0892197490, -0.0672501549, 0.0243918076, -0.0282466393, -0.0319776163, 0.0797045901, 0.0211536232};
+#endif
+
+#endif
